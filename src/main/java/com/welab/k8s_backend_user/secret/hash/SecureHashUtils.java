adc96c21ac287734e61c2bad552cec7fcfc597cc
wkddnjs9 <78907516+wkddnjs9@users.noreply.github.com>
2025-06-12 14:13:12 +0900

    0.0.5수정
    hash SHA-256 변환

--- src/main/java/com/welab/k8s_backend_user/secret/hash/SecureHashUtils.java
+++ src/main/java/com/welab/k8s_backend_user/secret/hash/SecureHashUtils.java
@@ -1,9 +1,23 @@
 package com.welab.k8s_backend_user.secret.hash;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class SecureHashUtils {
     public static String hash(String message) {
-        // TODO: message -> SHA-1 또는SHA-256 hash
-        return message;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = md.digest(message.getBytes(StandardCharsets.UTF_8));
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashedBytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Could not find SHA-256 algorithm", e);
+        }
     }
     public static boolean matches(String message, String hashedMessage) {
         String hashed = hash(message);
